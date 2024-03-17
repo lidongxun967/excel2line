7a86eecba67276dd4dd2751a9127fdfb384aa4fa
Debug967 <89326292+lidongxun967@users.noreply.github.com>
2024-03-17 23:22:56 +0800

    Update excel2line.py

--- excel2line.py
+++ excel2line.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.set_page_config("Excel转折线图",layout="wide",page_icon="♾️",initial_sidebar_state="expanded")
+st.set_page_config("Excel转折线图",layout="wide",page_icon="♾️"，)
 st.title('Excel转折线图')
 
 upf = st.file_uploader('导入excel文件',type=['xlsx','xls'])
@@ -14,6 +14,8 @@ dfs = pd.read_excel(upf,None)
 names = list(dfs.keys())
 
 tabs = st.tabs(names)
+
+tabs
 
 for tab,name in zip(tabs,names):
     with tab:
