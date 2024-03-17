8feb8d0eebf527ec795030b9c897dae6d337f5be
Debug967 <89326292+lidongxun967@users.noreply.github.com>
2024-03-17 19:32:29 +0800

    Update excel2line.py

--- excel2line.py
+++ excel2line.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.set_page_config("Excel转折线图",layout="wide",page_icon="♾️",initial_sidebar_state="collapsed")
+st.set_page_config("Excel转折线图",layout="wide",page_icon="♾️",initial_sidebar_state="expanded")
 st.title('Excel转折线图')
 
 upf = st.file_uploader('导入excel文件',type=['xlsx','xls'])
