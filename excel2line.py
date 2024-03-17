f9865b3eb5e96f7caf4e71474b2f3eccfde03515
Debug967 <89326292+lidongxun967@users.noreply.github.com>
2024-03-17 15:18:59 +0800

    Add files via upload

---
+++ excel2line.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+
+upf = st.file_uploader('导入excel文件',type=['xlsx','xls'])
+
+if not upf:
+    st.stop()
+    
+dfs = pd.read_excel(upf,None)
+
+names = list(dfs.keys())
+
+tabs = st.tabs(names)
+
+for tab,name in zip(tabs,names):
+    with tab:
+        df = dfs[name]
+        st.line_chart(df)
+
